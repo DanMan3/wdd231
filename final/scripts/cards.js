c3d2f8a9801b506b8292e19ee92b83acaa668641
DanMan3 <dhbennion@gmail.com>
2025-11-19 20:46:07 -0700

    Wasatch Tracker project files added.  Home page added.

---
+++ final/scripts/cards.js
@@ -0,0 +1,79 @@
+import { resorts } from "../data/resorts.mjs"
+
+
+const cards = document.querySelector(".cards")
+resorts.forEach((card, i) => {
+    const constructedCard = constructCard(card, i);
+    cards.appendChild(constructedCard);
+});
+
+
+
+
+function constructCard(card, i) {
+
+    const constructedCard = document.createElement("div")
+    constructedCard.className = "card"
+
+    const resortName = document.createElement("h2")
+    resortName.textContent = card.name;
+
+    const resortImage = document.createElement("img")
+    resortImage.src = card.image;
+    resortImage.alt = `${card.name} Ski Resort`
+    resortImage.loading = "lazy";
+    resortImage.width = "300";
+    resortImage.height = "200";
+
+
+    const resortText = document.createElement("p")
+    resortText.textContent = `${card.about} (Click for price info)`
+
+    const modals = document.querySelector(".modals")
+    const dialogElement = document.createElement("dialog")
+    dialogElement.id = `modal-${i}`
+    dialogElement.className = "modal"
+
+
+
+    constructedCard.appendChild(resortName)
+    constructedCard.appendChild(resortImage)
+    constructedCard.appendChild(resortText)
+    modals.appendChild(dialogElement)
+
+    constructedCard.addEventListener('click', () => {
+        displayModal(dialogElement, card)
+    })
+
+    return constructedCard
+
+}
+
+
+function displayModal(dialogElement, card) {
+
+    const prices = card.prices;
+
+    dialogElement.innerHTML = `<button id="close" aria-label="close">❌</button>
+      <h3>${card.name}</h3>
+      <h4>Tickets</h4> 
+      <p>Full Day: ${prices.day}</p>
+      <p>Half Day: ${prices.halfDay}</p>
+      <p>Night: ${prices.night}</p>
+      <hr></hr>
+      <h4>Season Passes</h4>
+      <p>Unlimited: ${prices.season}</p>
+      <p>Midweek: ${prices.seasonMidweek}</p>
+      <p>Nights Only: ${prices.seasonNight}</p>`;
+
+    const close = dialogElement.querySelector("#close")
+
+    dialogElement.showModal();
+
+    close.addEventListener('click', () => {
+        dialogElement.close();
+    })
+
+}
+
+
